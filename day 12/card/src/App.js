b89ad289874eaaff5b834e510998bda94659d9fb
Arunsahu07 <is5617916@gmail.com>
2021-07-03 19:03:20 +0530

    done 1 que(card)

---
+++ day 12/card/src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <img src={require("./a.jpg")} alt ='a'/>
+      <h1>Hello </h1>
+    </div>
+  );
+}

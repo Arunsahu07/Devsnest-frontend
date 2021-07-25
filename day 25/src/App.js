346d623ff6d15aad87231b743bfa930b3e9e89e2
Arunsahu07 <is5617916@gmail.com>
2021-07-25 09:35:34 +0530

    created login field and card using bootstrap-react

---
+++ day 25/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button,Alert,Breadcrumb, BreadcrumbItem} from 'react-bootstrap/';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MayCard from './component/card';
+import './App.css';
+export default function App()
+{
+  return <div>
+    
+    
+    <MayCard/>
+  </div>
+} 

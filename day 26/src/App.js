12f2fb6b2df3d003602813c616c248fa423ae844
Arunsahu07 <is5617916@gmail.com>
2021-07-27 22:26:28 +0530

    added styling

--- day 26/src/App.js
+++ day 26/src/App.js
@@ -1,13 +1,15 @@
 import { useDispatch, useSelector } from "react-redux";
 import { firstNameHandler,lastNameHandler } from "./actions/inputActions";
+import './App.css'
 export default function App()
 { const firstName = useSelector( (state)=>state.firstName );
   const lastName = useSelector( (state)=> state.lastName  );
   const dispatch=useDispatch();
   return <div>
+    <h1>Redux state mangement</h1>
     <input type='text' onChange={e=>{dispatch(firstNameHandler(e.target.value))}} placeholder='last-name'/>
     <input type='text'onChange={e=>{ dispatch(lastNameHandler(e.target.value))}} placeholder='last name'/>
-    <div> First name :   { firstName}</div>
-    <div> Last name :          {lastName}</div>
+    <div className='firstNameDiv'> First name :   { firstName}</div>
+    <div className='secondNameDiv'> Last name :          {lastName}</div>
   </div>
 }
